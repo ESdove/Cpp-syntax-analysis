3dc6de8df0fd929bd87de1c5b44ce3c4ef51f028
ESdove <cz18792083952@gmail.com>
2020-11-09 21:58:06 +0800

    封装

---
+++ 13-封装/13-封装/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Person
+{
+private:
+	int m_age;
+public:
+	void setAge(int age) {
+		if (age <= 0)
+		{
+			m_age = 1;
+		}
+		else
+		{
+			m_age = age;
+		}
+	}
+
+	int getAge() {
+		return m_age;
+	}
+};
+
+int main() {
+	Person person;
+	person.setAge(-4);
+
+	cout << person.getAge() << endl;
+	// person.m_age = -4;
+
+	getchar();
+	return 0;
+}
