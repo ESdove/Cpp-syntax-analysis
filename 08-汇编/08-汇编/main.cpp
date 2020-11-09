f8306cc621a7c265d6402a3d9ad7f3b890f972d0
ESdove <cz18792083952@gmail.com>
2020-11-09 11:24:13 +0800

    汇编分析

---
+++ 08-汇编/08-汇编/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+void test()
+{
+
+}
+
+void func()
+{
+	int a = 1;
+	int b = 2;
+	int c = a + b;
+}
+
+int sum(int a, int b)
+{
+	return a + b;
+}
+
+int main()
+{
+
+	int age = 3;
+
+	int &ref = age;
+	ref = 5;
+
+	getchar();
+	return 0;
+}
