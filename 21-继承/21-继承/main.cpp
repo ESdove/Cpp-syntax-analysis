909987992bb6a929b8b7176f86683e2c021edc04
ESdove <cz18792083952@gmail.com>
2020-11-10 18:33:33 +0800

    继承时对象的内存布局

---
+++ 21-继承/21-继承/main.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+//struct Person {
+//	int m_age;
+//	void run() { }
+//};
+//
+//struct Student : Person {
+//	int m_score;
+//	void study() { }
+//};
+//
+//struct Worker : Person {
+//	int m_salary;
+//	void work() { }
+//};
+
+// 基类：最基本的类。其他所有的类最终都会继承自它。类的老祖宗 
+
+struct Person
+{
+
+};
+
+struct Student : Person
+{
+	int m_no;
+};
+
+struct GoodStudent : Student
+{
+	int m_money;
+};
+
+int main()
+{
+
+	cout << sizeof(GoodStudent) << endl;
+
+	//GoodStudent gs; // 12
+	//gs.m_no = 2;
+	//gs.m_money = 3;
+
+	//GoodStudent gs2; // 12
+	//gs2.m_no = 2;
+
+	getchar();
+	return 0;
+}
