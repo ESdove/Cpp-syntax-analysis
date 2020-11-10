f944af79be9ca9e1f68dd81ff1993cb7962c6243
ESdove <cz18792083952@gmail.com>
2020-11-10 11:29:56 +0800

    内存管理

---
+++ 18-内存管理/18-内存管理/main.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Car
+{
+	int m_price;
+
+	Car()
+	{
+		m_price = 0;
+		cout << "Car::Car()" << endl;
+	}
+
+	~Car()
+	{
+		cout << "Car::~Car()" << endl;
+	}
+};
+
+struct Person
+{
+private:
+	int m_age;
+	Car *m_car;
+public:
+	// 用来做初始化的工作
+	Person()
+	{
+		m_age = 0;
+		m_car = new Car();
+
+		cout << "Person::Person()" << endl;
+	}
+
+	// 用来做内存清理的工作
+	~Person()
+	{
+		delete m_car;
+
+		cout << "Person::~Person()" << endl;
+	}
+};
+
+int main()
+{
+
+	// 内存泄露：该释放的内存并没有得到释放
+	/*{
+		Person person;
+	}*/
+
+	int age = 20;
+	int *p = &age;
+
+	getchar();
+	return 0;
+}
