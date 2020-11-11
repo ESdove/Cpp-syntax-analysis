1a46a4fbd8477e7b98ce29a8463edfc75d695759
ESdove <cz18792083952@gmail.com>
2020-11-11 15:38:25 +0800

    抽象类定义接口规范

---
+++ 28-纯虚函数/28-纯虚函数/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+// Java：抽象类、接口
+// OC：协议
+struct Animal
+{
+	virtual void speak() = 0;
+	virtual void run() = 0;
+};
+
+struct Dog : Animal
+{
+	void run()
+	{
+		cout << "Dog::run()" << endl;
+	}
+};
+
+struct Hashiqi : Dog
+{
+	void speak()
+	{
+		cout << "Hashiqi::speak()" << endl;
+	}
+};
+
+int main()
+{
+
+	getchar();
+	return 0;
+}
