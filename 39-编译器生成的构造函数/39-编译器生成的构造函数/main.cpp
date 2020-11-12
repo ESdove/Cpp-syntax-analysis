66533be1667a8c6ad24b782ea162149c884e77b9
ESdove <cz18792083952@gmail.com>
2020-11-12 14:20:36 +0800

    编译器生成的构造函数

---
+++ 39-编译器生成的构造函数/39-编译器生成的构造函数/main.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+很多教程都说：编译器会为每一个类都生成空的无参的构造函数
+*/
+
+//class Person {
+//public:
+//	int m_age = 0;
+//};
+//
+//class Student : public Person {
+//public:
+//
+//};
+
+
+//class Car {
+//public:
+//	int m_price = 0;
+//};
+//
+//class Person {
+//public:
+//	Car car;
+//};
+
+class Student
+{
+public:
+	int m_price;
+	void run()
+	{
+
+	}
+};
+
+int main()
+{
+
+	Student student;
+	student.run();
+
+	getchar();
+	return 0;
+}
