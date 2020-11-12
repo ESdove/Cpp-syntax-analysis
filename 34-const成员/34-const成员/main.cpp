22f6b6b68c9541d8468b9ce833cc0a508b10d6db
ESdove <cz18792083952@gmail.com>
2020-11-12 10:14:49 +0800

    const成员

---
+++ 34-const成员/34-const成员/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+class Car
+{
+public:
+	// const int m_price;
+	// Car() :m_price(0) {}
+	int m_price;
+
+	void run() const
+	{
+		cout << "run() const" << endl;
+	}
+
+	/*void run() {
+		cout << "run()" << endl;
+	}*/
+};
+
+int main()
+{
+	Car car;
+	car.run();
+
+	const Car car2;
+	car2.run();
+
+	getchar();
+	return 0;
+}
