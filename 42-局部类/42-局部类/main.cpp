d2f7d0e039c07dfcec6a56ed0cb3ad36a2a88d6b
ESdove <cz18792083952@gmail.com>
2020-11-12 14:24:57 +0800

    局部类

---
+++ 42-局部类/42-局部类/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int g_age = 10;
+
+
+
+void test()
+{
+	static int age = 10;
+
+	// 局部类
+	class Car
+	{
+	public:
+		void run()
+		{
+			age = 20;
+		}
+	};
+
+	Car car;
+	car.run();
+}
+
+
+
+
+int main()
+{
+
+
+	getchar();
+	return 0;
+}
