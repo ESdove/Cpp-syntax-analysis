4e7dcc5a9250c64e4321d93d84d95f02a32727ac
ESdove <cz18792083952@gmail.com>
2020-11-12 14:23:39 +0800

    内部类

---
+++ 41-内部类/41-内部类/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+class Person
+{
+private:
+	int m_age;
+
+public:
+	class Car
+	{
+	private:
+		int m_price;
+	};
+};
+
+int main()
+{
+	// Person::Car car1;
+	// Person::Car car2;
+
+	Person person;
+
+	getchar();
+	return 0;
+}
