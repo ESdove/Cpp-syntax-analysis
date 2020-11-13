d9bf28e4ce9935cd9143f53dcf74482f670c16e2
ESdove <cz18792083952@gmail.com>
2020-11-13 11:31:35 +0800

    模板

---
+++ 46-模板/46-模板/add.hpp
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+template <typename T>
+T add(T a, T b)
+{
+	return a + b;
+}
---
+++ 46-模板/46-模板/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include "add.hpp"
+
+using namespace std;
+
+class Point
+{
+	friend ostream &operator<<(ostream &, const Point &);
+	int m_x;
+	int m_y;
+public:
+	Point(int x, int y) :m_x(x), m_y(y) {}
+	Point operator+(const Point &point)
+	{
+		return Point(m_x + point.m_x, m_y + point.m_y);
+	}
+};
+
+ostream &operator<<(ostream &cout, const Point &point)
+{
+	return cout << "(" << point.m_x << ", " << point.m_y << ")";
+}
+
+int main()
+{
+
+	cout << add(10, 20) << endl; // call 函数地址
+	cout << add(1.5, 21.6) << endl; // call 函数地址
+	cout << add(Point(1, 2), Point(3, 4)) << endl;
+
+	// 编译、链接原理
+
+	getchar();
+	return 0;
+}
