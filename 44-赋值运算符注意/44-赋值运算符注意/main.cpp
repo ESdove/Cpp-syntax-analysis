d6218b7ce4ff943ebf12bdd905c76a88b776ea46
ESdove <cz18792083952@gmail.com>
2020-11-13 11:28:05 +0800

    赋值运算符注意

---
+++ 44-赋值运算符注意/44-赋值运算符注意/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+class Person
+{
+public:
+	int m_age;
+	Person &operator=(const Person &person)
+	{
+		m_age = person.m_age;
+	}
+};
+
+
+class Student : public Person
+{
+public:
+	int m_score;
+	Student &operator=(const Student &student)
+	{
+		Person::operator=(student);
+		m_score = student.m_score;
+	}
+};
+
+int main()
+{
+	Student stu1;
+	stu1.m_age = 20;
+	stu1.m_score = 100;
+
+	Student stu2;
+
+	stu2 = stu1;
+
+	getchar();
+	return 0;
+}
