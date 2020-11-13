8927112255365772aef37d31660678120593f582
ESdove <cz18792083952@gmail.com>
2020-11-13 11:29:32 +0800

    仿函数

---
+++ 45-仿函数/45-仿函数/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+//int sum(int a, int b) {
+//	return a + b;
+//}
+
+class Sum
+{
+public:
+	int m_age;
+	int operator()(int a, int b)
+	{
+		if (m_age)
+		{
+
+		}
+		return a + b;
+	}
+
+	void func()
+	{
+		m_age = 10;
+	}
+};
+
+int main()
+{
+
+	Sum sum;
+	sum.m_age = 10;
+	// sum.func();
+	cout << sum(10, 20) << endl;
+
+	// cout << sum.operator()(10, 20) << endl;
+
+	getchar();
+	return 0;
+}
