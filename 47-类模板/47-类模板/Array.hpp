d52c9b8e37664ec9ba6b6cfe7d2457f1133bcc96
ESdove <cz18792083952@gmail.com>
2020-11-13 11:33:25 +0800

    类模板

---
+++ 47-类模板/47-类模板/Array.hpp
@@ -0,0 +1,119 @@
+﻿#pragma once
+#include <iostream>
+using namespace std;
+
+template <typename Item>
+class Array
+{
+	friend ostream &operator<<<>(ostream &, const Array<Item> &);
+	// 用于指向首元素
+	Item *m_data;
+	// 元素个数
+	int m_size;
+	// 容量
+	int m_capacity;
+	void checkIndex(int index);
+public:
+	Array(int capacity = 0);
+	~Array();
+	void add(Item value);
+	void remove(int index);
+	void insert(int index, Item value);
+	Item get(int index);
+	int size();
+	Item operator[](int index);
+};
+
+template <typename Item>
+Array<Item>::Array(int capacity)
+{
+	m_capacity = (capacity > 0) ? capacity : 10;
+
+	// 申请堆空间
+	m_data = new Item[m_capacity];
+}
+
+template <typename Item>
+Array<Item>::~Array()
+{
+	if (m_data == NULL) return;
+	delete[] m_data;
+}
+
+template <typename Item>
+void Array<Item>::checkIndex(int index)
+{
+	if (index < 0 || index >= m_size)
+	{
+		// 报错：抛异常
+		throw "数组下标越界";
+	}
+}
+
+template <typename Item>
+void Array<Item>::add(Item value)
+{
+	if (m_size == m_capacity)
+	{
+		// 扩容
+		/*
+		1.申请一块更大的新空间
+		2.将旧空间的数据拷贝到新空间
+		3.释放旧空间
+		*/
+		cout << "空间不够" << endl;
+		return;
+	}
+
+	m_data[m_size++] = value;
+}
+
+template <typename Item>
+void Array<Item>::remove(int index)
+{
+	checkIndex(index);
+
+}
+
+template <typename Item>
+void Array<Item>::insert(int index, Item value)
+{
+
+}
+
+template <typename Item>
+Item Array<Item>::get(int index)
+{
+	checkIndex(index);
+
+	return m_data[index];
+}
+
+template <typename Item>
+int Array<Item>::size()
+{
+	return m_size;
+}
+
+template <typename Item>
+Item Array<Item>::operator[](int index)
+{
+	return get(index);
+}
+
+template <typename Item>
+ostream &operator<<<>(ostream &cout, const Array<Item> &array)
+{
+	cout << "[";
+
+	for (int i = 0; i < array.m_size; i++)
+	{
+		if (i != 0)
+		{
+			cout << ", ";
+		}
+		cout << array.m_data[i];
+	}
+
+	return cout << "]";
+}
---
+++ 47-类模板/47-类模板/main.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include "Array.hpp"
+
+using namespace std;
+
+class Point
+{
+	friend ostream &operator<<(ostream &, const Point &);
+	int m_x;
+	int m_y;
+public:
+	Point(int x = 0, int y = 0) :m_x(x), m_y(y) {}
+};
+
+ostream &operator<<(ostream &cout, const Point &point)
+{
+	return cout << "(" << point.m_x << ", " << point.m_y << ")";
+}
+
+class Person
+{
+
+};
+
+class Car
+{
+
+};
+
+int main()
+{
+	Array<Point> array;
+	array.add(Point(1, 2));
+	array.add(Point(3, 4));
+
+	array.remove(0);
+	array.get(0);
+
+
+	array.insert(2, 33);
+
+	cout << array << endl;
+
+	/*Array<int> array;
+	array.add(10);
+	array.add(20);
+	array.add(30);
+
+	cout << array << endl;*/
+	// [10, 20, 30]
+
+	/*Array<void *> array;
+	array.add(&Point(1, 2));
+	array.add(new Point(3, 4));
+	array.add(new Car());*/
+
+	/*cout << array.get(0) << endl;
+	cout << array.size() << endl;*/
+
+
+	getchar();
+	return 0;
+}
